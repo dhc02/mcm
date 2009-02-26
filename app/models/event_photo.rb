ac31a5307e01338510f17aaa70ebfa88381526f3
U-DHC-LAPTOP\donniec <donniec@dhc-Laptop.(none)>
2009-02-25 21:08:24 -0700

    checking out for the night

--- app/models/event_photo.rb
+++ app/models/event_photo.rb
@@ -2,6 +2,6 @@ class EventPhoto < ActiveRecord::Base
   
   belongs_to :event
   
-  has_attached_file :photo, :styles => { :medium => "300x300#", :thumb => "100x100#" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100#" }
   
 end
