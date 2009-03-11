f99fa2a748d6ac01e4ffd58d32d33b7df327cf97
U-DHC-LAPTOP\donniec <donniec@dhc-Laptop.(none)>
2009-03-11 01:29:35 -0600

    deleted old index controller/view, began restful_auth journey

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090311070848) do
+ActiveRecord::Schema.define(:version => 20090311072518) do
 
   create_table "event_photos", :force => true do |t|
     t.string   "photo_file_name"
@@ -56,5 +56,19 @@ ActiveRecord::Schema.define(:version => 20090311070848) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
