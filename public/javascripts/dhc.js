cdd892a2e8149f9130e867c45e4ec3456a0a1c9c
Donnie Clapp <dhclapp@gmail.com>
2009-03-13 12:51:24 -0600

    needed to escape the href variable in my javascript

--- public/javascripts/dhc.js
+++ public/javascripts/dhc.js
@@ -17,7 +17,8 @@ jQuery(document).ready(function() {
   $("img").each(function (){
     if ($(this).parent().is("a") != true) {
       var href = $(this).attr("src");
-      $(this).wrap("<a rel='highslide' class='highslide' href='" + href + "' ></a>");
+      var replaced = escape(href);
+      $(this).wrap("<a rel='highslide' class='highslide' href='" + replaced + "' ></a>");
     }
   });
 
