279510b5560e22b7db78232059a0c9064f58aa10
Donnie Clapp <dhclapp@gmail.com>
2010-01-14 22:54:38 -0800

    periodic update

--- public/javascripts/dhc.js
+++ public/javascripts/dhc.js
@@ -1,4 +1,5 @@
 //Highslide stuff
+
 hs.graphicsDir = '/images/highslide/graphics/';
 hs.registerOverlay({
   		thumbnailId: null,
@@ -9,30 +10,41 @@ hs.registerOverlay({
 hs.outlineType = 'rounded-white';
 hs.showCredits = false;
 
+//jQuery Stuff
 
 jQuery(document).ready(function() {
   
-  // hide flash messages after 2 seconds
+  // hide flash messages after 2 seconds (not actually animating opacity, just using the delay function built into 'animate' to delay 2 secs before doing slideUp)
+  
   $("#flash div").animate({opacity: 1.0}, 2000).slideUp("slow");
   
-  // add highslide to all images that aren't already wrapped in a link (to avoid double-highsliding)
+  // add highslide to all images unless they're already wrapped in a link (to avoid double-highsliding)
+  
   $("img").each(function (){
     if ($(this).parent().is("a") != true) {
+      //grab the src url from the image tag to use as the href for the wrapping link tag
       var href = $(this).attr("src");
+      //url escape that url due to likely spaces and other chars in the path
       var replaced = escape(href);
+      //wrap the img tag in a new a tag
       $(this).wrap("<a rel='highslide' class='highslide' href='" + replaced + "' ></a>");
     }
   });
   
   // add expand/contract functionality to p.subhead sections
+  
   var link = "<a class='plus' href='#'>[+/-]</a>";
+  //hide the next element on the page after any p.subhead and then append the +/- link to the end of each p.subhead
   $("p.subhead").next().hide("slow").end().append(link);
+  //now clicking a +/- link should expand the hidden section following it. The link's parent is the p.subhead.
   $("a.plus").click(function (){
     $(this).parent().next().toggle("fast");          
     return false;
   });
 	  
 
+  //Ajax loading of subpages (not compatible with Rails version of site as of 6/09. There is overlap with p.subhead expand/contract section above, so take heed if attempting to reimplement.)
+  
   // $("a.nav").click(function (){
   //   var dest = $(this).attr("id");
   //   var link = "<a class='plus' href='#'>[+/-]</a>";
